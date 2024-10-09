a368a1587d4cf9813c3a0e3b81a7daea486848ee
DeadDarkus <forov.mp@yandex.ru>
2024-10-09 12:31:35 +0300

    Update setup-geo.bat

--- setup-geo.bat
+++ setup-geo.bat
@@ -1,5 +1,5 @@
 curl -OL https://github.com/deaddarkus4/ru-block-sing_box-rules/releases/latest/download/geoip-ru-block.srs
-wget -OL https://github.com/deaddarkus4/ru-block-sing_box-rules/releases/latest/download/geosite-ru-block.srs 
+curl -OL https://github.com/deaddarkus4/ru-block-sing_box-rules/releases/latest/download/geosite-ru-block.srs 
 copy geosite-ru-block.srs "%1%\bin\srss"
 copy geoip-ru-block.srs "%1%\bin\srss"
 del /Q geosite-ru-block.srs
